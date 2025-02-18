706660e9fbd8b662d4a7702b24699b4fb7218fed
221B167 <221B167@juet.co.in>
2025-02-18 11:22:01 +0530

    LE3

--- LE2/q1/Test.java
+++
@@ -1,8 +0,0 @@
-public class Test {
-    public static void main(String[] args) {
-        // Create a Voice object and prepare the voices
-        Voice voice = new Voice();
-        voice.prepareVoice();  // This will call the prepareVoice and then hear methods
-    }
-}
-
